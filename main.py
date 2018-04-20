e086426fd8ba9cacb80528559655da28ec75e52d
Saif Siraj <saifasiraj@gmail.com>
2018-04-20 04:10:11 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 #GAPP
 import webapp2
+from google.appengine.ext import ndb
+
+#UID , Name , URL , Description , Deadline , Date , and Tags.
+class opportunities(ndb.Model):
+    uid = ndb.IntegerProperty()
+    name = ndb.StringProperty()
+    url = ndb.StringProperty()
+    description = ndb.StringProperty()
+    deadline = ndb.DateProperty()
+    date = ndb.Dateproperty()
+    tags = ndb.StringProperty()
+
+#<input name="csv_file" type="file" id="myFile">
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
