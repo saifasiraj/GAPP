5e66d2dbb799ad194434f9c3cb539e78fbdd62d8
Saif Siraj <saifasiraj@gmail.com>
2018-04-21 19:10:15 -0400

    Finished Context1
    parsed csv and creates objects using the ndb object called opportunities. When the calculations are done and the objects are stored it redirects to a new page for a table of opps (under listpage class NOT SET UP)

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #GAPP
 import webapp2
 import cgi, os
-import cgidb
+import cgitb
+import jinja2
+import csv
+
 from google.appengine.ext import ndb
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 #UID , Name , URL , Description , Deadline , Date , and Tags.
-class opportunities(ndb.Model):
+class Opportunities(ndb.Model):
     uid = ndb.StringProperty()
     name = ndb.StringProperty()
     url = ndb.StringProperty()
@@ -18,14 +26,57 @@ class opportunities(ndb.Model):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('testing')
-        csvfile = self.request.get('file')
-        print csvfile
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
 
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        csvfile = self.request.get('file')
+        filelines = csvfile.splitlines()
+
+        headercheck = 0;
+        for lines in filelines:
+            if(headercheck == 0):
+                headercheck+=1
+                continue
+
+            reader = csv.reader([lines])
+            for mainarr in reader:
+                opp = Opportunities()
+                if(not mainarr[0]):
+                    mainarr[0] = mainarr[1].replace(' ', '-')
+                opp.uid = mainarr[0]
+                opp.name = mainarr[1]
+                opp.url = mainarr[2]
+                opp.description = mainarr[3]
+                opp.deadline = mainarr[4]
+                opp.date = mainarr[5]
+                opp.tags = mainarr[6]
+                opp.put()
+
+
+        self.response.write("DONE")
+        self.redirect('/opportunities')
+
+class ListPage(webapp2.RequestHandler):
+        def get(self):
+            self.response.write("DONE2323")
+
+    #    for lines in filelines:
+    #        opp = Opportunities()
+    #        if(lines[0] == ','):
+    #            noUID = 1
+    #        items = lines.split(',')
+    #        self.response.write(items)
+    #        self.response.write("\n")
+    #        self.response.write(lines)
+    #        self.response.write("\n")
+    #        self.response.write("\n")
 
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/opportunities', ListPage)
 ], debug=True)
